3b88bb6d3478f7b37cf9ae7bc30585a4b403945e
omni <96026994+hoo12F@users.noreply.github.com>
2022-07-02 10:40:40 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+# Random Number Generator in Python using the Random Standard Library
+import random
+mode = input("Choose a mode, multiplayer or AI: ")
+if mode == 'AI':
+    choose = int(input("Choose a number 1-10: "))
+    print(choose)
+
+    opponent = random.randint(1,10)
+    print(int(opponent))
+
+    if opponent > choose: 
+        print("You lost! ")
+    if opponent < choose:
+        print("You won! ")
+    if opponent == choose:
+        print("Tie!")
+    if choose > 10:
+        print("Invalid!")
+
+if mode == 'multiplayer':
+    name1 = input("What is your name, Player 1?: ")
+    name2 = input("What is your name, Player 2?: ")
+    player1 = int(input(name1 + " choose a number 1-10: "))
+    player2 = int(input(name2 + " choose a number 1-10: "))
+
+    if player1 > player2:
+        print(name1 + " wins!")
+
+    if player1 < player2:
+        print(name2 + " wins! ")
+
+    if player1 > 10:
+        print("Invalid!")
+    if player2 > 10:
+        print("Invalid")
+
+    if player1 == player2:
+        print("Tie!")
