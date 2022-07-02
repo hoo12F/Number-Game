30e0cfb614d49b856664e301fda4640e0121f79f
soof <baominhtalt@outlook.com>
2022-07-02 18:13:52 +0000

    New Features

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import random
 mode = input("Choose a mode, multiplayer or AI: ")
 if mode == 'AI':
     choose = int(input("Choose a number 1-10: "))
+    if choose > 10:
+        print("Invalid!")
+        quit()
     print(choose)
 
     opponent = random.randint(1,10)
@@ -14,14 +17,18 @@ if mode == 'AI':
         print("You won! ")
     if opponent == choose:
         print("Tie!")
-    if choose > 10:
-        print("Invalid!")
 
 if mode == 'multiplayer':
     name1 = input("What is your name, Player 1?: ")
     name2 = input("What is your name, Player 2?: ")
     player1 = int(input(name1 + " choose a number 1-10: "))
+    if player1 > 10:
+        print("Invalid!")
+        quit()
     player2 = int(input(name2 + " choose a number 1-10: "))
+    if player2 > 10:
+        print("Invalid!")
+        quit()
 
     if player1 > player2:
         print(name1 + " wins!")
@@ -29,10 +36,7 @@ if mode == 'multiplayer':
     if player1 < player2:
         print(name2 + " wins! ")
 
-    if player1 > 10:
-        print("Invalid!")
-    if player2 > 10:
-        print("Invalid")
+
 
     if player1 == player2:
         print("Tie!")
