cb8eee768b1d9476460cee7a02f6240a700f629a
soof <baominhtalt@outlook.com>
2022-07-03 21:14:26 +0000

    new mode

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ A python game using python's random standard library.
 2. On the bottom, click download zip.
 3. Download python
 4. Finally, go to the zip you just downloaded and press on main.py
+
+## Simple Guide
+- **Modes**
+    - **Randomized**: A game of pure luck where your number and your opponent's is randomized
+    - **AI**: Play against the computer
+    - **Multiplayer**: Play against a friend
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # Random Number Generator in Python using the Random Standard Library
 import random
-mode = input("Choose a mode, multiplayer or AI: ")
+print("\nWelcome! \n\n")
+mode = input("Choose a mode: ")
 if mode == 'AI':
     choose = int(input("Choose a number 1-10: "))
     if choose > 10:
         print("Invalid!")
         quit()
+    print("Your number: \n")
     print(choose)
 
     opponent = random.randint(1,10)
+    print("Opponent's number: \n")
     print(int(opponent))
 
     if opponent > choose: 
@@ -29,7 +32,10 @@ if mode == 'multiplayer':
     if player2 > 10:
         print("Invalid!")
         quit()
-
+    print(name1 + "'s number: \n")
+    print(player1)
+    print(name2 + "'s number: \n")
+    print(player2)
     if player1 > player2:
         print(name1 + " wins!")
 
@@ -40,3 +46,41 @@ if mode == 'multiplayer':
 
     if player1 == player2:
         print("Tie!")
+if mode == 'randomized':
+    choice = input("Do you want to play against the computer or a friend?: ")
+
+    if choice == 'computer':
+        computer = int(random.randint(1,10))
+        player = int(random.randint(1,10))
+        print("Your Number: \n")
+        print(player)
+        print("Opponent's Number: \n")
+        print(computer)
+        if computer > player:
+            print("computer wins!")
+        if computer < player:
+            print("you win!")
+
+        if computer == player:
+            print("tie!")
+    if choice == 'friend':
+        player_name1 = input("What is your name, player 1?: ")
+        player_name2  =input("What is your name, player 2?: ")
+        player1number = random.randint(1,10)
+        player2number = random.randint(1,10)
+
+        print(player_name1 + "'s number is: \n")
+        print(player1number)
+        print(player_name2 + "'s number is: \n")
+        print(player2number)
+
+        if player1number > player2number:
+            print(player_name1 + " wins!")
+
+        if player1number < player2number:
+            print(player_name2 + " wins!")
+
+        if player1number == player2number:
+            print("Tie!")
+
+        
